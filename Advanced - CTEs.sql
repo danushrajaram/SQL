e43b05a3a5b88b5665e88ba47ce338a6ac346eef
Danush Rajaram <134055382+danushrajaram@users.noreply.github.com>
2025-11-28 18:29:04 +0530

    Update Advanced - CTEs.sql

--- Advanced - CTEs.sql
+++ Advanced - CTEs.sql
@@ -3,11 +3,8 @@
 -- It is particularly useful for recursive queries or queries that require referencing a higher level
 -- this is something we will look at in the next lesson/
 
--- Let's take a look at the basics of writing a CTE:
 
 
--- First, CTEs start using a "With" Keyword. Now we get to name this CTE anything we want
--- Then we say as and within the parenthesis we build our subquery/table we want
 WITH CTE_Example AS 
 (
 SELECT gender, SUM(salary), MIN(salary), MAX(salary), COUNT(salary), AVG(salary)
@@ -45,14 +42,14 @@ GROUP BY gender;
 
 
 
--- we also have the ability to create multiple CTEs with just one With Expression
+-- to create multiple CTEs with just one With Expression
 
 WITH CTE_Example AS 
 (
 SELECT employee_id, gender, birth_date
 FROM employee_demographics dem
 WHERE birth_date > '1985-01-01'
-), -- just have to separate by using a comma
+), 
 CTE_Example2 AS 
 (
 SELECT employee_id, salary
@@ -66,10 +63,8 @@ LEFT JOIN CTE_Example2 cte2
 	ON cte1. employee_id = cte2. employee_id;
 
 
--- the last thing I wanted to show you is that we can actually make our life easier by renaming the columns in the CTE
--- let's take our very first CTE we made. We had to use tick marks because of the column names
+--  renaming the columns in the CTE
 
--- we can rename them like this
 WITH CTE_Example (gender, sum_salary, min_salary, max_salary, count_salary) AS 
 (
 SELECT gender, SUM(salary), MIN(salary), MAX(salary), COUNT(salary)
